d3061c845e45510d8e6bcef765e20560cc906faa
Eugene Wang <eugenewyj79@gmail.com>
2017-03-26 21:32:10 +0800

    添加golang blog示例代码

---
+++ golangblog/httptracing/tracingevents.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptrace"
+	"fmt"
+	"log"
+)
+
+func main() {
+	req, _ := http.NewRequest("GET", "http://www.baidu.com", nil)
+	trace :=  &httptrace.ClientTrace{
+		DNSDone: func(dnsDoneInfo httptrace.DNSDoneInfo) {
+			fmt.Printf("DNS Info: %+v\n", dnsDoneInfo)
+		},
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			fmt.Printf("Got Conn: %+v\n", connInfo)
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+	if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+		log.Fatal(err)
+	}
+}
