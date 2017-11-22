be0c862e569bbb7a7da595d4ac05051638dd79d9
Eugene Wang <fengbaoxp@gmail.com>
2017-11-22 12:32:35 +0800

    添加REST

--- grpc-sample/client/main.go
+++ grpc-sample/client/main.go
@@ -7,12 +7,42 @@ import (
 
 	"github.com/eugenewyj/eugene-go-samples/grpc-sample/api"
 	"golang.org/x/net/context"
+	"google.golang.org/grpc/credentials"
 )
+
+// Authentication holds the login/password
+type Authentication struct {
+	Login    string
+	Password string
+}
+
+// GetRequestMetadata 获取当前请求元数据
+func (a *Authentication) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"login":    a.Login,
+		"password": a.Password,
+	}, nil
+}
+
+// RequireTransportSecurity 标识是否认证需要安全传输
+func (a *Authentication) RequireTransportSecurity() bool {
+	return true
+}
 
 func main() {
 	var conn *grpc.ClientConn
 
-	conn, err := grpc.Dial(":7777", grpc.WithInsecure())
+	creds, err := credentials.NewClientTLSFromFile("cert/server.crt", "")
+	if err != nil {
+		log.Fatalf("不能加载tls cert: %s", err)
+	}
+
+	auth := Authentication{
+		Login:    "eugene",
+		Password: "wang",
+	}
+
+	conn, err = grpc.Dial("localhost:7777", grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(&auth))
 	if err != nil {
 		log.Fatalf("连接失败： %s", err)
 	}
--- grpc-sample/server/main.go
+++ grpc-sample/server/main.go
@@ -4,29 +4,145 @@ import (
 	"fmt"
 	"net"
 
+	"strings"
+
+	"log"
+
+	"net/http"
+
 	"github.com/eugenewyj/eugene-go-samples/grpc-sample/api"
-	"github.com/ngaut/log"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 )
+
+// private type for Context keys
+type contextKey int
+
+const clientIDKey contextKey = iota
+
+func credMatcher(headerName string) (mdName string, ok bool) {
+	if headerName == "Login" || headerName == "Password" {
+		return headerName, true
+	}
+	return "", false
+}
+
+// 认证代理，检查登录的客户信息
+func authenticateClient(ctx context.Context, server *api.Server) (string, error) {
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		clientLogin := strings.Join(md["login"], "")
+		clientPassword := strings.Join(md["password"], "")
+		if clientLogin != "eugene" {
+			return "", fmt.Errorf("不合法的用户： %s", clientLogin)
+		}
+		if clientPassword != "wang" {
+			return "", fmt.Errorf("密码错误：%s", clientPassword)
+		}
+
+		log.Printf("合法客户： %s", clientLogin)
+		return "42", nil
+	}
+
+	return "", fmt.Errorf("无认证信息")
+}
+
+// unaryInterceptor 采用当前context调用authenticateClient
+func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	s, ok := info.Server.(*api.Server)
+	if !(ok) {
+		return nil, fmt.Errorf("unable to cast server")
+	}
+	clientID, err := authenticateClient(ctx, s)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx = context.WithValue(ctx, clientIDKey, clientID)
+	return handler(ctx, req)
+}
+
+func startGRPCServer(address, certFile, keyFile string) error {
+	// create a listener on TCP port
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("监听接口失败： %v", err)
+	}
+
+	// 创建一个服务实例
+	server := api.Server{}
+
+	creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+	if err != nil {
+		return fmt.Errorf("不能加载TLS keys: %s", err)
+	}
+
+	// 创建一个带证书参数的gRPC options
+	opts := []grpc.ServerOption{grpc.Creds(creds), grpc.UnaryInterceptor(unaryInterceptor)}
+
+	grpcServer := grpc.NewServer(opts...)
+
+	api.RegisterPingServer(grpcServer, &server)
+
+	// 启动服务
+	log.Printf("启动HTTP/2 gRPC server on %s", address)
+	if err := grpcServer.Serve(listener); err != nil {
+		return fmt.Errorf("启动服务失败： %s", err)
+	}
+	return nil
+}
+
+func startRESTServer(address, grpcAddress, certFile string) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	mux := runtime.NewServeMux(runtime.WithIncomingHeaderMatcher(credMatcher))
+
+	creds, err := credentials.NewClientTLSFromFile(certFile, "")
+	if err != nil {
+		return fmt.Errorf("不能加载TLS cretifiate: %s", err)
+	}
+
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+
+	err = api.RegisterPingHandlerFromEndpoint(ctx, mux, grpcAddress, opts)
+	if err != nil {
+		return fmt.Errorf("不能注册服务器： %s", err)
+	}
+
+	log.Printf("启动HTTP/1.1 REST server on %s", address)
+	http.ListenAndServe(address, mux)
+
+	return nil
+}
 
 // 启动一个gRPC服务，并等待连接
 func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
-	if err != nil {
-		log.Fatalf("监听接口失败：%v", err)
-	}
+	grpcAddress := fmt.Sprintf("%s:%d", "localhost", 7777)
+	restAddress := fmt.Sprintf("%s:%d", "localhost", 7778)
+	certFile := "cert/server.crt"
+	keyFile := "cert/server.key"
 
-	//创建一个Server实例
-	s := api.Server{}
+	// fire the gRPC server in a goroutine
+	go func() {
+		err := startGRPCServer(grpcAddress, certFile, keyFile)
+		if err != nil {
+			log.Fatalf("启动gRPC服务失败： %s", err)
+		}
+	}()
 
-	// 创建一个gRPC服务实例
-	grpcServer := grpc.NewServer()
+	// fire the REST server in a goroutine
+	go func() {
+		err := startRESTServer(restAddress, grpcAddress, certFile)
+		if err != nil {
+			log.Fatalf("启动REST服务失败： %s", err)
+		}
+	}()
 
-	// 绑定api服务到grpc服务
-	api.RegisterPingServer(grpcServer, &s)
-
-	// 启动服务
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("启动服务失败： %s", err)
-	}
+	// 无限循环
+	log.Printf("进入无限循环")
+	select {}
 }
